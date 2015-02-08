ad45b38bbe8cecb8eca342688e772f1b27e9b8ba
Sliim <sliim@mailoo.org>
2015-02-08 04:56:04 +0100

    Minor code improvements

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,23 +2,23 @@
 
 This file is used to list changes made in each version of bricks cookbook.
 
-## 0.1.0:
+## 0.1.4:
 
-* Initial release of bricks
+* Changed license for Apache 2.0
+
+## 0.1.3:
+
+* Integration testing with serverspec and test-kitchen
+
+## 0.1.2:
+
+* Added Atrai and Barak codename support.
 
 ## 0.1.1:
 
 * Added Phalgu codename support.
 * Added a bit of readme.
 
-## 0.1.2:
+## 0.1.0:
 
-* Added Atrai and Barak codename support.
-
-## 0.1.3:
-
-* Integration testing with serverspec and test-kitchen
-
-## 0.1.4:
-
-* Changed license for Apache 2.0
+* Initial release of bricks
--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,30 +1,32 @@
 # -*- coding: utf-8 -*-
 
 require 'serverspec'
+set :backend, :exec
 
-include SpecInfra::Helper::Exec
-include SpecInfra::Helper::DetectOS
-
-describe service('apache2') do
+describe service 'apache2' do
   it { should be_enabled }
   it { should be_running }
 end
 
-describe file('/etc/apache2/sites-available/bricks.conf') do
+describe port 80 do
+  it { should be_listening }
+end
+
+describe file '/etc/apache2/sites-available/bricks.conf' do
   it { should be_file }
   it { should be_owned_by 'root' }
   it { should be_grouped_into 'root' }
   it { should be_mode 644 }
 end
 
-describe file('/etc/apache2/sites-enabled/bricks.conf') do
+describe file '/etc/apache2/sites-enabled/bricks.conf' do
   it { should be_file }
   it { should be_owned_by 'root' }
   it { should be_grouped_into 'root' }
   it { should be_linked_to '/etc/apache2/sites-available/bricks.conf' }
 end
 
-describe file('/opt/bricks') do
+describe file '/opt/bricks' do
   it { should be_directory }
   it { should be_owned_by 'root' }
   it { should be_grouped_into 'root' }
