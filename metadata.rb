22b15a3bde6463b1b157724e7f76e5ee0bf85106
Sliim <sliim@mailoo.org>
2017-09-23 18:44:07 +0200

    Fix foodcritic

--- metadata.rb
+++ metadata.rb
@@ -16,9 +16,10 @@
 name 'bricks'
 maintainer 'Sliim'
 maintainer_email 'sliim@mailoo.org'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Installs/Configures Bricks application'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+chef_version '>= 12.5' if respond_to?(:chef_version)
 version '0.2.1'
 
 recipe 'default', 'Installs/configures bricks webapp + db'
@@ -31,3 +32,8 @@ depends 'database'
 
 supports 'ubuntu'
 supports 'debian'
+
+source_url 'https://github.com/wargames-cookbooks/bricks' if
+  respond_to?(:source_url)
+issues_url 'https://github.com/wargames-cookbooks/bricks/issues' if
+  respond_to?(:issues_url)
